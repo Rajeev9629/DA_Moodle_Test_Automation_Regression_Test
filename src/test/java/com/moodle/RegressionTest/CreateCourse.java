3fd49a809cff57a1764cca1e31c6f4e9cb4ed864
rajeev9629 <rajeev.rm@happiestminds.com>
2021-01-24 23:20:22 +0530

    Commiting new regression folder and adding scripts to create course and enroll users into the new course

---
+++ src/test/java/com/moodle/RegressionTest/CreateCourse.java
@@ -0,0 +1,26 @@
+package com.moodle.RegressionTest;
+
+import java.util.LinkedHashMap;
+
+import org.testng.annotations.Test;
+
+import com.framework.utils.Utility;
+import com.moodle.tlh.tests.BaseTest;
+import com.netsuite.tlh.operations.Navigator;
+import com.netsuite.tlh.testdata.CreateBackupData;
+
+public class CreateCourse  extends BaseTest{
+	
+	@Test(description = "MFD-221 ::MFD-222 ::MFD-470::MFD-515::Create a backup and Restore for the course,Verify FM Dashboard Link", dataProvider = "getData", dataProviderClass = com.netsuite.tlh.dataprovider.NetsuiteTLHTestDataProvider.class)
+	public void CreateABackupAndRestoreTheCourse(LinkedHashMap<String, ?> testData) throws Throwable {
+		loggingStartReport("MFD-221 ::MFD-222 ::Create a backup and Restore");
+		CreateBackupData createBackupData = Utility.getDataPojo(testData.get("Form"), CreateBackupData.class);
+		
+		rightNavOperations.verifyFacilitationManagerDashboard().getRestoreCoursePage();
+		Navigator.doRestore(createBackupData);
+		rightNavOperations.clickHome();
+		
+		
+	}
+
+}
